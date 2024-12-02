a650e161231e6a24bfef02cb0efa5a8c85ae371a
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-02 18:58:20 -0300

    test(change): change structure to tests

--- __tests__/integration/api/v1/migrations/get.spec.ts
+++ __tests__/integration/api/v1/migrations/get.spec.ts
@@ -1,11 +1,22 @@
-import { waitForAllServices } from "../../../../orchestrator";
+import {
+  clearMigrationTable,
+  waitForAllServices,
+} from "../../../../orchestrator";
 
-beforeAll(async () => await waitForAllServices());
+beforeAll(async () => {
+  await waitForAllServices();
+  await clearMigrationTable();
+});
 
-test("Retrieving pending migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
-  const responseJson = await response.json();
-  expect(Array.isArray(responseJson)).toBeTruthy();
-  expect(responseJson.length).toBe(0);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+
+      const responseJson = await response.json();
+      expect(Array.isArray(responseJson)).toBeTruthy();
+      expect(responseJson.length).toBeGreaterThan(0);
+    });
+  });
 });
