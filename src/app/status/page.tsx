613754597e9b062d95d5423e22ecc94a1dc69c0c
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-09 13:52:15 -0300

    refactor: change status page handling for server

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,49 +1,30 @@
-"use client";
 import { fetchAPI } from "@/shared/lib/http/http";
-import useSWR from "swr";
 
-function UpdatedAt(props: { updatedAt: string }) {
-  return (
-    <div>
-      Ultima atualização da página foi{" "}
-      {new Date(props.updatedAt).toLocaleString("pt-BR", {
-        dateStyle: "full",
-        timeStyle: "medium",
-      })}
-      .
-    </div>
-  );
+async function getStatus() {
+  const { ok, data } = await fetchAPI("/v1/status");
+  if (ok) return data;
+  throw data;
 }
 
-function DatabaseStatus(props: {
-  version: string;
-  maxConnections: number;
-  openedConnections: number;
-}) {
+export default async function Page() {
+  const data = await getStatus();
   return (
-    <div>
-      <h2>Database</h2>
-      <div>Versão do banco de dados {props.version}</div>
-      <div>Máximo de conexões {props.maxConnections}</div>
-      <div>Conexões abertas {props.openedConnections}</div>
-    </div>
-  );
-}
-
-export default function Page() {
-  const { isLoading, data } = useSWR({ path: "/api/v1/status" }, fetchAPI, {
-    refreshInterval: 20000,
-  });
-  if (isLoading) return <h1>Loading...</h1>;
-  return (
-    <div>
+    <section>
       <h1>Status</h1>
-      <UpdatedAt updatedAt={data?.updated_at}></UpdatedAt>
-      <DatabaseStatus
-        version={data?.dependencies?.database?.version}
-        openedConnections={data?.dependencies?.database?.opened_connections}
-        maxConnections={data?.dependencies?.database?.max_connections}
-      ></DatabaseStatus>
-    </div>
+      <p>
+        Ultima atualização da página foi{" "}
+        {new Date(data.updated_at).toLocaleString("pt-BR", {
+          dateStyle: "full",
+          timeStyle: "medium",
+        })}
+        .
+      </p>
+      <article>
+        <h2>Database</h2>
+        <p>Versão do banco de dados {data.dependencies.database.version}</p>
+        <p>Máximo de conexões {data.dependencies.database.maxConnections}</p>
+        <p>Conexões abertas {data.dependencies.database.openedConnections}</p>
+      </article>
+    </section>
   );
 }
--- src/shared/lib/http/http.ts
+++ src/shared/lib/http/http.ts
@@ -1,11 +1,20 @@
+import { headers } from "next/headers";
+
 export enum HttpCodes {
   OK = 200,
   Created = 201,
+  NotFound = 404,
   MethodNotAllowed = 405,
   InternalServerError = 500,
 }
 
-export async function fetchAPI(props: { path: string; options?: RequestInit }) {
-  const response = await fetch(props.path, props.options);
-  return await response.json();
+export async function fetchAPI(path: string, options?: RequestInit) {
+  const header = await headers();
+  const baseUrl = `${header.get("x-forwarded-proto") ?? "http"}://${header.get("host")!}/api`;
+  const response = await fetch(`${baseUrl}${path}`, options);
+  return {
+    ok: response.ok,
+    status: response.status,
+    data: await response.json(),
+  };
 }
