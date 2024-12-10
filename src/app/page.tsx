a8074069138b50e5421afaac56ce1a2db3ad56ef
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-10 20:32:21 -0300

    refactor: change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import React from "react";
+
 export default function Home() {
-  return <h1> Em Construção! </h1>;
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <header>
+        <h1 className="text-3xl font-bold text-gray-700">Em Construção</h1>
+      </header>
+      <p className="text-gray-500 mt-4 text-center max-w-xs">
+        Esta área está em desenvolvimento. Em breve, traremos novidades!
+      </p>
+    </section>
+  );
 }
