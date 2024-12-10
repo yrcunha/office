5213c8680889256ac3d2e0c1648ff1dbcf5f1ec5
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-10 20:27:15 -0300

    refactor: add `tailwindcss` style and add links

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,13 +1,36 @@
-import headerStyles from "./header.module.css";
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
-    <header>
-      <nav id={headerStyles.navbar}>
-        <Link href="/">Home</Link>
-        <Link href="/status">Status</Link>
+    <header className="bg-white">
+      <nav className="flex p-8 justify-between items-center border-b border-lightgray max-w-7xl mx-auto">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/resources" className="hover:underline">
+          Recursos
+        </Link>
+        <Link href="/plans" className="hover:underline">
+          Planos e Preços
+        </Link>
+        <Link href="/about" className="hover:underline">
+          Sobre o JurisAgenda
+        </Link>
+        <div className="flex space-x-4">
+          <Link href="/login" className="hover:underline">
+            <button className="bg-gray-500 text-white py-2 px-6 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400">
+              Acessar Sistema
+            </button>
+          </Link>
+          <Link href="/register">
+            <button className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
+              Experimente o Sistema
+            </button>
+          </Link>
+        </div>
       </nav>
     </header>
   );
