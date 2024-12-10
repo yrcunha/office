df6cda92c2501336ecf60aa228c77f67c4ecaff8
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-10 20:28:35 -0300

    refactor: change `layout.tsx` to `use client` and add `tailwindcss` style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import Header from "@/shared/components/header/header";
-import "./global.css";
+"use client";
 
-export const metadata = {
-  title: "Office",
-  description: "Office",
-};
+import React from "react";
+import "./global.css";
+import Header from "@/shared/components/header/header";
+import { inter } from "@/shared/lib/fonts/inter";
 
 export default function RootLayout({
   children,
@@ -14,9 +12,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>
+      <head>
+        <title>Office</title>
+        <meta name="description" content="Office" />
+      </head>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
-        <main>{children}</main>
+        <main className="flex-grow flex items-center justify-center">
+          {children}
+        </main>
       </body>
     </html>
   );
