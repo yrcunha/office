da3604c1fdcd292e081332d2491283c341c8a5ec
Yuri Rodrigues <yrcunha@gmail.com>
2024-12-14 10:49:01 -0300

    refactor: change `orchestrator.ts` and reflect the changes in the tests

--- __tests__/integration/api/v1/migrations/get.spec.ts
+++ __tests__/integration/api/v1/migrations/get.spec.ts
@@ -1,22 +1,16 @@
-import {
-  clearMigrationTable,
-  waitForAllServices,
-} from "../../../../orchestrator";
+import { BaseUrl, waitForAllServices } from "../../../../orchestrator";
+import { HttpCodes } from "../../../../../src/shared/lib/http/http";
 
-beforeAll(async () => {
-  await waitForAllServices();
-  await clearMigrationTable();
-});
+beforeAll(async () => await waitForAllServices());
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/migrations");
-      expect(response.status).toBe(200);
-
+      const response = await fetch(`${BaseUrl}/v1/migrations`);
+      expect(response.status).toBe(HttpCodes.OK);
       const responseJson = await response.json();
       expect(Array.isArray(responseJson)).toBeTruthy();
-      expect(responseJson.length).toBeGreaterThan(0);
+      expect(responseJson.length).toEqual(0);
     });
   });
 });
--- __tests__/orchestrator.ts
+++ __tests__/orchestrator.ts
@@ -1,11 +1,16 @@
 import retry from "async-retry";
 import { ServiceUnavailabilityError } from "../src/shared/lib/errors/service-unavailability.error";
 import { query } from "../src/features/services/datasource/database";
+import type { RunnerOption } from "node-pg-migrate";
+import { runner } from "node-pg-migrate";
+import { join } from "node:path";
+
+export const BaseUrl = "http://localhost:3000/api";
 
 export function waitForAllServices() {
   return retry(
     async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
+      const response = await fetch(`${BaseUrl}/v1/status`);
       if (response.ok) return;
       throw new ServiceUnavailabilityError(
         "Services unavailable to start the test battery!",
@@ -19,4 +24,23 @@ export function clearMigrationTable() {
   return query(
     "drop table if exists pgmigrations; drop sequence if exists pgmigrations_id_seq;",
   );
+}
+
+export function runningMigrations() {
+  const defaultMigrationOptions: RunnerOption = Object.freeze({
+    databaseUrl: process.env.DATABASE_URL!,
+    dryRun: false,
+    dir: join(
+      process.cwd(),
+      "src",
+      "features",
+      "services",
+      "datasource",
+      "migrations",
+    ),
+    direction: "up",
+    verbose: false,
+    migrationsTable: "pgmigrations",
+  });
+  return runner(defaultMigrationOptions);
 }
